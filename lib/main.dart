b441c6c65c0ed132213578222bdd1f3af6d7a91c
Fardeen Khan <khanfardeen9983@gmail.com>
2019-09-04 12:15:43 +0530

    Firebase Setup with other dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,57 +1,25 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:genesis19_publicity/pages/home_page.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    int _counter = 0;
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Provider Demo"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+    return MultiProvider(
+      providers: [
+        StreamProvider<FirebaseUser>.value(
+          value: FirebaseAuth.instance.onAuthStateChanged,
+        )
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
         ),
-      ),
-      floatingActionButton: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: <Widget>[
-          FloatingActionButton(
-            onPressed: () {},
-            tooltip: 'Increment',
-            child: Icon(Icons.add),
-          ),
-          SizedBox(height: 10),
-          FloatingActionButton(
-            onPressed: () {},
-            tooltip: 'Increment',
-            child: Icon(Icons.remove),
-          )
-        ],
+        home: HomePage(),
       ),
     );
   }
---
+++ lib/model/participant.dart
@@ -0,0 +1,12 @@
+class Participant {
+  final String name;
+  final int mobile;
+  final String dept;
+  final int year;
+  final List<int> events;
+  final String email;
+  final List<String> reciepts;
+
+  Participant(this.name, this.mobile, this.dept, this.year, this.events,
+      this.email, this.reciepts);
+}
